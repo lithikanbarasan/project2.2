71368c9e3cef690fd82ef9d952a86ccb1920a5f0
lithikanbarasan <156556502+lithikanbarasan@users.noreply.github.com>
2025-05-14 15:50:45 -0700

    Create queue_tester.c

---
+++ apps/queue_tester.c
@@ -0,0 +1,18 @@
+void test_create(void)
+{
+    queue_t q;
+
+    q = queue_create();
+    assert(q != NULL);
+}
+
+void test_queue_simple(void)
+{
+    queue_t q;
+    int data = 3, *ptr;
+
+    q = queue_create();
+    queue_enqueue(q, &data);
+    queue_dequeue(q, (void**)&ptr);
+    assert(ptr == &data);
+}
